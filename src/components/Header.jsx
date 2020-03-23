9077e54b34b8d736b78e550a8766770428ea22c5
Dmitry <purpl3mint@yandex.ru>
2020-03-23 20:42:25 +0700

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src='https://img2.freepng.ru/20180601/fbh/kisspng-black-mesa-research-facility-portal-aperture-labor-5b1154aef17ff0.7098908915278624469892.jpg' />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://1.bp.blogspot.com/-ykXOZ0JQMPo/XcIIkIke_MI/AAAAAAAAgTk/SBLPUdZsFbAtlqknkXyDUAG7cdY6-Hq7ACLcBGAsYHQ/s640/city-of-bright-lights-ca-2560x1440.jpg' />
+      </div>
+      <div>
+        ava + description
+        </div>
+      <div>
+        My posts
+          <div>
+          New post
+          </div>
+        <div>
+          Post 1
+          </div>
+        <div>
+          Post 2
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
