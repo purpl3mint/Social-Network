0553504ab963574d573ae3da79ce14efdd6b51c0
Dmitry <purpl3mint@yandex.ru>
2020-03-29 12:50:55 +0700

    Store and observer added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import * as serviceWorker from './serviceWorker';
-import { rerenderEntireTree } from './render.js';
-import state from './redux/state';
+import store from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-rerenderEntireTree(state);
+export let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        appState={store.getState()}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+rerenderEntireTree(store.state);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,7 @@
-import { rerenderEntireTree } from "../render";
+/*
+let rerenderEntireTree = () => {
+  console.log();
+}
 
 let state = {
   profilePage: {
@@ -25,7 +28,7 @@ let state = {
   }
 }
 
-export let addPost = () => {
+export const addPost = () => {
   let newPost = {
     id: 5,
     message: state.profilePage.newPostText,
@@ -37,10 +40,71 @@ export let addPost = () => {
   rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
+  rerenderEntireTree();
 }
 
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
+}
 
 export default state;
+*/
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, message: 'How are you?', likesCount: 5 },
+        { id: 2, message: 'hi', likesCount: 9 },
+        { id: 3, message: 'YAY! THIS IS MY POST!!!', likesCount: 6 }
+      ],
+      newPostText: 'KeK'
+    },
+
+    dialogsPage: {
+      dialogsData: [
+        { id: 1, name: 'Vanya' },
+        { id: 2, name: 'Masha' },
+        { id: 3, name: 'Petya' }
+      ],
+
+      messagesData: [
+        { id: 1, text: 'Hi' },
+        { id: 2, text: 'How are you?' },
+        { id: 3, text: ':D' }
+      ]
+    }
+  },
+
+  _callSubscriber() {
+    console.log();
+  },
+
+  getState() {
+    return this._state;
+  },
+
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0
+    };
+
+    this._state.profilePage.newPostText = '';
+    this._state.profilePage.postsData.push(newPost);
+    this._callSubscriber();
+  },
+
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber();
+  },
+
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
+}
+
+export default store;
