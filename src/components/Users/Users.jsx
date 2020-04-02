45206301710b22ca1eff902781f0ec8531b4a5fe
Dmitry <purpl3mint@yandex.ru>
2020-04-02 14:33:29 +0700

    Take users from server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
 import style from './users.module.css';
+import * as axios from 'axios';
 
 let Users = (props) => {
 
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoURL: 'https://speedflip.ru/temp/avatar/34511avatar.jpeg',
-        followed: true,
-        fullname: 'Ivan',
-        status: 'Look at this DUUUUDE',
-        location: { city: 'Moscow', country: 'Russia' }
-      },
-      {
-        id: 2,
-        photoURL: 'https://cdn1.flamp.ru/2833daf619c6711113e8c572955aede8_100_100.jpg',
-        followed: false,
-        fullname: 'Masha',
-        status: 'Hola!',
-        location: { city: 'Minsk', country: 'Belarus' }
-      },
-      {
-        id: 3,
-        photoURL: 'https://im0-tub-ru.yandex.net/i?id=68e86db384b4959374e9d37d1f17c492&n=16',
-        followed: false,
-        fullname: 'Andrew',
-        status: 'HeheHE',
-        location: { city: 'Kiev', country: 'Ukraine' }
-      }
-    ]);
+    axios.get("https://social-network.samuraijs.com/api/1.0/users")
+      .then(response => {
+        props.setUsers(response.data.items);
+      });
+    /*
+        props.setUsers([
+          {
+            id: 1,
+            photoURL: 'https://speedflip.ru/temp/avatar/34511avatar.jpeg',
+            followed: true,
+            fullname: 'Ivan',
+            status: 'Look at this DUUUUDE',
+            location: { city: 'Moscow', country: 'Russia' }
+          },
+          {
+            id: 2,
+            photoURL: 'https://cdn1.flamp.ru/2833daf619c6711113e8c572955aede8_100_100.jpg',
+            followed: false,
+            fullname: 'Masha',
+            status: 'Hola!',
+            location: { city: 'Minsk', country: 'Belarus' }
+          },
+          {
+            id: 3,
+            photoURL: 'https://im0-tub-ru.yandex.net/i?id=68e86db384b4959374e9d37d1f17c492&n=16',
+            followed: false,
+            fullname: 'Andrew',
+            status: 'HeheHE',
+            location: { city: 'Kiev', country: 'Ukraine' }
+          }
+        ]);
+        */
   }
 
   return <div>
@@ -37,7 +44,7 @@ let Users = (props) => {
       props.users.map(u => <div key={u.id} className={style.userContainer}>
         <span>
           <div>
-            <img src={u.photoURL} className={style.userPhoto} />
+            <img src={u.photos.small != null ? u.photos.small : "https://yt3.ggpht.com/a/AATXAJzaqQ27AzdLIzhcQjJyZSE-7bzisejMwGeevw=s900-c-k-c0xffffffff-no-rj-mo"} className={style.userPhoto} />
           </div>
           <div>
             {u.followed ?
@@ -47,12 +54,12 @@ let Users = (props) => {
         </span>
         <span className={style.userInfo}>
           <span className={style.username}>
-            <div>{u.fullname}</div>
+            <div>{u.name}</div>
             <div>{u.status}</div>
           </span>
           <span>
-            <div>{u.location.city}</div>
-            <div>{u.location.country}</div>
+            <div>{'u.location.city'}</div>
+            <div>{'u.location.country'}</div>
           </span>
         </span>
       </div>)
