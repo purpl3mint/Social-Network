c2e13ea828e9e73ce9791353468153249fd3227a
Dmitry <purpl3mint@yandex.ru>
2020-04-13 00:09:55 +0700

    Added auth query to server

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import { BrowserRouter, Route } from 'react-router-dom';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 function App(props) {
   return (
 
     <div className='app-wrapper'>
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className='app-wrapper-content'>
         <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import style from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={style.header}>
       <img src='https://img2.freepng.ru/20180601/fbh/kisspng-black-mesa-research-facility-portal-aperture-labor-5b1154aef17ff0.7098908915278624469892.jpg' />
+      <div className={style.loginBlock}>
+        {props.isAuth ? props.login
+          : <NavLink to={'/login'}>Login</NavLink>
+        }
+      </div>
     </header>
   );
 }
