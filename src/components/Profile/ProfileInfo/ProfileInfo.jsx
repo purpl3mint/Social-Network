78be9ca3584d55271706c2d2add2a2054146562e
Dmitry <purpl3mint@yandex.ru>
2020-04-19 15:34:42 +0700

    Added status in profile page

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus.jsx';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -9,13 +10,15 @@ const ProfileInfo = (props) => {
 
   return (
     <div>
-      <div>
-        <img src='https://1.bp.blogspot.com/-ykXOZ0JQMPo/XcIIkIke_MI/AAAAAAAAgTk/SBLPUdZsFbAtlqknkXyDUAG7cdY6-Hq7ACLcBGAsYHQ/s640/city-of-bright-lights-ca-2560x1440.jpg' />
-      </div>
+      {
+        /*<div>
+          <img src='https://1.bp.blogspot.com/-ykXOZ0JQMPo/XcIIkIke_MI/AAAAAAAAgTk/SBLPUdZsFbAtlqknkXyDUAG7cdY6-Hq7ACLcBGAsYHQ/s640/city-of-bright-lights-ca-2560x1440.jpg' />
+        </div>*/
+      }
       <div className={style.descriptionBlock}>
         <img src={props.profile.photos.large} />
-        ava + description
-        </div>
+        <ProfileStatus status="Hola" />
+      </div>
     </div>
   );
 }
